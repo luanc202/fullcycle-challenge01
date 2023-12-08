09947d7551dc427f5e107e0ef436570acaa66aab
Luan A. <barkon32@tuta.io>
2023-12-08 20:20:09 -0300

    csv reading working

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
-package desafio01go
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("data.csv")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	reader := csv.NewReader(file)
+	records, _ := reader.ReadAll()
+	fmt.Println(records)
+
+}
